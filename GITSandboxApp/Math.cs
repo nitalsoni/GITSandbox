7262f77702117344fd5c9148db69f64558511ee6
Nital Soni <nital.soni@gmail.com>
2020-05-24 00:18:07 +0530

    Mod added

---
+++ GITSandboxApp/Math.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GITSandboxApp
+{
+    class Math
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Sub(int a, int b)
+        {
+            return a - b;
+        }
+
+        public int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+
+        public int Divide(int a, int b)
+        {
+            return a / b;
+        }
+
+        public int Mod(int a, int b)
+        {
+            return a % b;
+        }
+    }
+}
