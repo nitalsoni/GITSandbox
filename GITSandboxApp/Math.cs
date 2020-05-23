2147e38682d309050b4dfd29236acf42502990d4
Nital Soni <nital.soni@gmail.com>
2020-05-23 23:41:43 +0530

    Adding Math class with basic Ops

---
+++ GITSandboxApp/Math.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GITSandboxApp
+{
+    class Math
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Sub(int a, int b)
+        {
+            return a - b;
+        }
+
+        public int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+
+        public int Divide(int a, int b)
+        {
+            return a / b;
+        }
+    }
+}
