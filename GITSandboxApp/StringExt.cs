656394ce95e6fa397a9ebee87a2f4c18f0bf2e22
Nital Soni <nital.soni@gmail.com>
2020-05-25 18:55:01 +0530

    Adding String features

---
+++ GITSandboxApp/StringExt.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GITSandboxApp
+{
+    class StringExt
+    {
+        public string AltUpper(string s)
+        {
+            return s.ToUpper();
+        }
+
+        public override string ToString()
+        {
+            return "StringExt";
+        }
+    }
+}
